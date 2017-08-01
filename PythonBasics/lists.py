67c492cad0709b83cd845a93da39aaaddd47d79b
Thomas B Klapheke <tommyklapheke@yahoo.com>
2017-08-01 14:11:12 -0400

    Wrapped up the Strings section of Python and started on Lists

---
+++ PythonBasics/lists.py
@@ -0,0 +1,1 @@
+#Understanding the lists of Python
--- PythonBasics/strings.py
+++ PythonBasics/strings.py
@@ -1,4 +1,4 @@
-# Understanding the strings of Python
+# Understanding the use of strings of Python
 
 stuff = "stuff"
 print(stuff)
@@ -17,3 +17,18 @@ It's right here!"""
 print(long_string)
 
 print(str(7))
+
+print("Hello" + " there!")
+
+name = "Tommy "
+name += "Klapheke"
+print(name)
+
+print('=' * 40)
+
+status_message = "Hey, we have {} people using the site right now"
+print(status_message.format(8))
+print(status_message.format(7))
+
+message = "We have {} people with {} kids tonight"
+print(message.format(7, 3))
