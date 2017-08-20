ee69988ea3376ebd50fc67e0e0149c34ea4c53b5
Thomas B Klapheke <tommyklapheke@yahoo.com>
2017-08-20 13:06:20 -0400

    Added new functions for better refining later on and reusability later on

--- shopping_list_v2.py
+++ shopping_list_v2.py
@@ -11,13 +11,29 @@ def show_help():
     Enter 'SHOW' to see what items you have in your list
     """)
 
+def show_list():
+    print("Here's your list: ")
+
+    for item in shopping_list:
+        print(item)
+
+def add_to_list(new_item):
+    shopping_list.append(new_item)
+    print("Added {}. List now has {} items.".format(new_item, len(shopping_list)))
+
+show_help()
+
 while True:
     new_item = input("> ")
     if new_item == "DONE":
         break
-    shopping_list.append(new_item)
+    elif new_item == 'HELP':
+        show_help()
+        continue
+    elif new_item == "SHOW":
+        show_list()
+        continue
+    add_to_list(new_item)
 
-print("Here's your list: ")
+show_list()
 
-for item in shopping_list:
-    print(item)
