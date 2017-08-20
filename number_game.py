d091b6ceb9da4b83fb87c9954f3812725f4ee251
Thomas B Klapheke <tommyklapheke@yahoo.com>
2017-08-20 17:00:45 -0400

    Added some additional features: -Limit the number of guess -Catch non-integers -Play again -Too low guess and too high guess

--- number_game.py
+++ number_game.py
@@ -3,13 +3,35 @@
 
 import random
 
-secret_num = random.randint(1, 10)
+def game():
+    secret_num = random.randint(1, 10)
+    guesses = []
 
-while True:
-    guess = int(input("Guess a number between 1 and 10: "))
-
-    if guess == secret_num:
-        print("You got it!. Your number was {}!".format(secret_num))
-        break
+    while len(guesses) < 5:
+        try:
+            guess = int(input("Guess a number between 1 and 10: "))
+        except ValueError:
+            print("{} isn't a number!".format(guess))
+        else:
+            if guess == secret_num:
+                print("You got it!. Your number was {}!".format(secret_num))
+                break
+            elif guess < secret_num:
+                print("My number is higher than {}".format(guess))
+            elif guess > secret_num:
+                print("My number is lower than {}".format(guess))            
+            else:
+                print("That's not it!")
+            guesses.append(guess)
+            
     else:
-        print("That's not it!")
+        print("You didn't get it! My number was {}".format(secret_num))
+        
+    play_again = input("Do you want to play again? Y/n")
+    
+    if play_again.lower() != 'n':
+        game()
+    else:
+        print("Bye!")
+
+game()
